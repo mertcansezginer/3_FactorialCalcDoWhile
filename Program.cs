eb34bb8d63791e9e122b462ec84370de6b832083
mertc <mertc@DESKTOP-8ID65LJ>
2024-03-06 14:47:50 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿
+Console.WriteLine("Faktoriyelini hesaplamak istediginiz sayiyi giriniz");
+int sayi = Convert.ToInt32(Console.ReadLine());  //kullanicidan sayi alindi
+long faktoriyel = 1;   //baslangic degerlerini belirlendi
+int i = 1;
+
+do
+{
+    faktoriyel *= i;    //faktoriyel hesaplama islemi
+    i++;
+}
+while (i <= sayi);
+
+Console.WriteLine(sayi + " sayisinin faktoriyeli " + faktoriyel);
